d4f69efd3811aec720300049db93a6615dddbf7c
Cade Gordon <cadegordon093@gmail.com>
2021-08-16 16:58:21 -0500

    Completed first exercise

---
+++ HelloDungeon/Game.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HelloDungeon
+{
+    class Game
+    {
+        public void Run()
+        {
+            //start menu
+            Console.WriteLine("Welcome to the game");
+            Console.WriteLine("Type start to continue");
+            Console.ReadLine();
+
+
+            //Choose player name
+            string name = "Empty";
+            Console.WriteLine("Please enter your name.");
+            name = Console.ReadLine();
+            Console.WriteLine("Welcome " + name);
+        }
+    }
+}
