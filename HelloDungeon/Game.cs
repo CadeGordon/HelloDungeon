0fe98ee68e583bb2ae9001cf732c50f5390cc785
Cade Gordon <cadegordon093@gmail.com>
2021-08-18 13:01:29 -0500

    added character select and show stats

--- HelloDungeon/Game.cs
+++ HelloDungeon/Game.cs
@@ -8,10 +8,31 @@ namespace HelloDungeon
     {
         public void Run()
         {
+            //health exercise
+            int health = 100 / 2;
+            int healthRegen = 50;
+            health = health + healthRegen;
+            health += healthRegen;
+            Console.WriteLine(health);
+
+            //character stats Gunslinger
+            string characterjob = "Gunslinger";
+            string characterhealth = "100";
+            string power = "200";
+
+            
+
+
+
+            
+            
+            
+
+
             //start menu
             Console.WriteLine("Welcome to the game");
             Console.WriteLine();
-            Console.WriteLine("Type start to continue");
+            Console.WriteLine("Press enter to continue");
             Console.ReadLine();
 
 
@@ -22,8 +43,43 @@ namespace HelloDungeon
             name = Console.ReadLine();
             Console.WriteLine();
             Console.WriteLine("Welcome " + name);
+            Console.WriteLine();
 
-            //test
+            //Character select menu
+            Console.WriteLine("Choose a class");
+            Console.WriteLine("1.Gunslinger");
+            Console.WriteLine("2.Brawler");
+            Console.Write("> ");
+
+            //input from player
+            string input = Console.ReadLine();
+
+            if (input == "1" || input == "Gunslinger")
+            {
+                characterjob = "Gunslinger";
+                characterhealth = "100";
+                power = "200";
+                Console.WriteLine("Gunslinger");
+                Console.WriteLine("Health 100");
+                Console.WriteLine("Power 200");
+                Console.ReadLine();
+                Console.Clear();
+                
+
+            }
+            else if (input == "2" || input == "Brawler")
+            {
+                characterjob = "Brawler";
+                characterhealth = "300";
+                power = "600";
+                Console.WriteLine("Brawler");
+                Console.WriteLine("Health 300");
+                Console.WriteLine("Power 600");
+                Console.ReadLine();
+                Console.Clear();
+
+            }
+
         }
     }
 }
