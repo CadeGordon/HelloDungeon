055fc36801758bdf17834de12ba4f854c74250b4
Cade Gordon <cadegordon093@gmail.com>
2021-08-23 15:19:16 -0500

    veriosn 1.0

--- HelloDungeon/Game.cs
+++ HelloDungeon/Game.cs
@@ -17,7 +17,7 @@ namespace HelloDungeon
         {
             Console.WriteLine("Choose a story");
             Console.WriteLine("1.Gunslinger");
-            Console.WriteLine("2.Brawler");
+            Console.WriteLine();
             Console.Write("> ");
             string input = "";
             int inputrecived = 0;
@@ -30,8 +30,98 @@ namespace HelloDungeon
             Console.WriteLine("1.Yes");
             Console.WriteLine("2.No");
             Console.Write("> ");
-            string input = "";
-            int inputrecived = 0;
+            if (input == "1")
+            {
+
+
+            }
+            else if (input == "2")
+            {
+                
+            }
+            
+
+            
+            
+            return 0;
+        }
+        int EncounterOne(string description, string option1, string option2)
+        {
+            
+            Console.WriteLine("you are drinking at a bar in a town you have never been to.");
+            Console.WriteLine();
+            Console.WriteLine("press enter to continue");
+            Console.WriteLine();
+            Console.ReadLine();
+            Console.WriteLine("4 men wearing bandanas all walk up behind you and stop, telling you to get out of their town");
+            Console.WriteLine();
+            Console.WriteLine("press enter to continue");
+            Console.ReadLine();
+            Console.WriteLine("how do you respond?");
+            Console.WriteLine();
+            Console.WriteLine("1.Attack the bandits");
+            Console.WriteLine("2.Apologize and walk away");
+            Console.Write("> ");
+            
+
+            return 0;
+        }
+        int EncounterOnePart2(string description, string option1, string option2)
+        {
+            Console.WriteLine("You throw ur whiskey on the tallest bandit and throw your chair at his head");
+            Console.WriteLine();
+            Console.WriteLine("The second bandit charges at you, but throw your whiskey glass at his head shattering the glass in his eyes");
+            Console.WriteLine();
+            Console.WriteLine("The third bandit slams a chair over your back knocking you to the ground in a daze you reach for your gun but the bandit throws you over the bar counter");
+            Console.WriteLine();
+            Console.WriteLine("Press enter to continue");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("you regain your composure how do you respond");
+            Console.WriteLine();
+            Console.WriteLine("1.Kill the remaining banits");
+            Console.WriteLine("2.Try and talk it out");
+            Console.Write("> ");
+            return 0;
+        }
+        int EncounterOnePart3(string description)
+        {
+            Console.WriteLine("You draw your guns shooting the bandits in the knee caps dropping them to ground killing them with a second shot to the head");
+            Console.WriteLine();
+            Console.WriteLine("Press enter to continue");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("The law rushes over to see what the sound was and see you holding two revolvers standing over 4 dead bodies");
+            Console.WriteLine();
+            Console.WriteLine("you get arrested you fail!!!");
+            Console.WriteLine();
+            Console.WriteLine("Press enter to continue");
+            Console.ReadLine();
+            Console.Clear();
+            return 0;
+        }
+        int Encounter2(string description)
+        {
+            Console.WriteLine("You say you are sorry and walk out the bar");
+            Console.WriteLine();
+            Console.WriteLine("Press enter to continue");
+            Console.ReadLine();
+            Console.WriteLine("you win because you made the right choice dont kill bandits");
+            return 0;
+        }
+        int Stats(string description, string healthstats, string powerstats)
+        {
+            
+            Console.WriteLine("Gunslinger");
+            Console.WriteLine("Health 100");
+            Console.WriteLine("Power 200");
+            Console.WriteLine("Two six shooter revolvers");
+            Console.WriteLine();
+            Console.WriteLine("press enter to continue");
+            Console.ReadLine();
+            Console.Clear();
+            validinputrecieved = true;
+            Console.Clear();
             return 0;
         }
 
@@ -101,7 +191,7 @@ namespace HelloDungeon
             while (!(inputrecieved == 1 || inputrecieved == 2))
             {
                 //Character select menu
-                GetInput("Choose a story", "1.Gunslinger", "2.Brawler");
+                GetInput("Choose a story", "1.Gunslinger", "");
                 
                 
 
@@ -111,80 +201,35 @@ namespace HelloDungeon
                 //gunslinger
                 if (input == "1" || input == "Gunslinger")
                 {
-                    characterjob = "Gunslinger";
-                    health = 100;
-                    power = 200;
-                    Console.WriteLine("Gunslinger");
-                    Console.WriteLine("Health 100");
-                    Console.WriteLine("Power 200");
-                    Console.WriteLine("Two six shooter revolvers");
-                    Console.WriteLine();
-                    Console.WriteLine("press enter to continue");
-                    Console.ReadLine();
-                    Console.Clear();
-                    validinputrecieved = true;
-                    Console.Clear();
+                    Stats("description", "healthstats", "powerstats");
 
                     // gunslinger story
                     if (input == "1" || input == "Gunslinger")
                     {
                         string decide = " ";
-                        Console.WriteLine("you are drinking at a bar in a town you have never been to.");
-                        Console.WriteLine();
-                        Console.WriteLine("press enter to continue");
-                        Console.WriteLine();
-                        Console.ReadLine();
-                        Console.WriteLine("4 men wearing bandanas all walk up behind you and stop, telling you to get out of their town");
-                        Console.WriteLine();
-                        Console.WriteLine("press enter to continue");
-                        Console.ReadLine();
-                        Console.WriteLine("how do you respond?");
-                        Console.WriteLine();
-                        Console.WriteLine("1.Attack the bandits");
-                        Console.WriteLine("2.Apologize and walk away");
-                        Console.Write("> ");
+                        //starts encounter 1 choices
+                        EncounterOne("description", "option1", "option2");
                         decide = Console.ReadLine();
                         Console.Clear();
 
                         if (decide == "1")
                         {
-                            Console.WriteLine("You throw ur whiskey on the tallest bandit and throw your chair at his head");
-                            Console.WriteLine();
-                            Console.WriteLine("The second bandit charges at you, but throw your whiskey glass at his head shattering the glass in his eyes");
-                            Console.WriteLine();
-                            Console.WriteLine("The third bandit slams a chair over your back knocking you to the ground in a daze you reach for your gun but the bandit throws you over the bar counter");
-                            Console.WriteLine();
-                            Console.WriteLine("Press enter to continue");
-                            Console.ReadLine();
-                            Console.Clear();
-                            Console.WriteLine("you regain your composure how do you respond");
-                            Console.WriteLine();
-                            Console.WriteLine("1.Kill the remaining banits");
-                            Console.WriteLine("2.Try and talk it out");
-                            Console.Write("> ");
+                            //start encounter 2 choices
+                            EncounterOnePart2("description", "option1", "option2");
                             decide = Console.ReadLine();
                             Console.Clear();
 
                             if (decide == "1")
                             {
-                                Console.WriteLine("You draw your guns shooting the bandits in the knee caps dropping them to ground killing them with a second shot to the head");
-                                Console.WriteLine();
-                                Console.WriteLine("Press enter to continue");
-                                Console.ReadLine();
-                                Console.Clear();
-                                Console.WriteLine("The law rushes over to see what the sound was and see you holding two revolvers standing over 4 dead bodies");
-                                Console.WriteLine();
-                                Console.WriteLine("you get arrested you fail!!!");
-                                Console.WriteLine();
-                                Console.WriteLine("Press enter to continue");
-                                Console.ReadLine();
-                                Console.Clear();
+                                //starts encounter 3 choices
+                                EncounterOnePart3("description");
+                                //game over needs work
                                 GameOver("1", "1", "1");
 
                             }
                             else if (decide == "2")
                             {
-                                Console.WriteLine("You try and talk but the law comes");
+                                Console.WriteLine("You try and talk but the law comes arresting the bandits");
                             }
                             else
                             {
@@ -194,29 +239,18 @@ namespace HelloDungeon
                         }
                         else if (decide == "2")
                         {
-                            Console.WriteLine("other stuff");
+                            Encounter2("");
+                            GameOver("","","");
+
+
                         }
                             
                     }
+                    
+                    
+                       
+                    
 
-                }
-                else if (input == "2" || input == "Brawler")
-                {
-                    characterjob = "Brawler";
-                    health = 300;
-                    power = 600;
-                    Console.WriteLine("Brawler");
-                    Console.WriteLine("Health 300");
-                    Console.WriteLine("Power 600");
-                    Console.WriteLine();
-                    Console.WriteLine("press enter to continue");
-                    Console.ReadLine();
-                    Console.Clear();
-                    validinputrecieved = true;
-
-                    //brawler story
-                    if (input == "2" || input == "Brawler")
-                        Console.WriteLine("Stuff happens");
                 }
                 else
                 {
